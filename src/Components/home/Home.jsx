fa1164488a167f77a7d3061577186954d9e80c8f
deXcoder9 <dhrubothejuniorprogrammer@gmail.com>
2024-04-30 00:23:38 +0600

    art and craft categories section added

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,3 +1,4 @@
+import ArtnCraftCategory from "../Art and Craft Categories/ArtnCraftCategory";
 import Form from "../extra two route/Form";
 import Testimonial from "../extra two route/Testimonial";
 import Banner from "./Banner";
@@ -11,8 +12,10 @@ const Home = () => {
            <Banner></Banner>
            
            <CraftIteams></CraftIteams>
+           <ArtnCraftCategory></ArtnCraftCategory>
            <Testimonial></Testimonial>
            <Form></Form>
+
         </div>
     );
 };
