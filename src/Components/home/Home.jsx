4eb2ececfe08ec677ff0641be4800eda9b015ce6
deXcoder9 <dhrubothejuniorprogrammer@gmail.com>
2024-04-30 11:07:15 +0600

    some changes are made

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -4,7 +4,7 @@ import Form from "../extra two route/Form";
 import CraftIteams from "./CraftIteams";
 import Banner from "./Banner";
 import { InfiniteMovingCardsDemo } from "./InfiniteMovingCards";
-import NewCraftItems from "./NewCraftItems";
+// import NewCraftItems from "./NewCraftItems";
 // import  InfiniteMovingCardsDemo  from "./InfiniteMovingCardsDemo";
 
 
@@ -20,8 +20,8 @@ const Home = () => {
         <div className="min-h-screen">
            <Banner></Banner>
            
-           {/* <CraftIteams></CraftIteams> */}
-           <NewCraftItems></NewCraftItems>
+           <CraftIteams></CraftIteams>
+           {/* <NewCraftItems></NewCraftItems> */}
            <ArtnCraftCategory></ArtnCraftCategory>
            
            <InfiniteMovingCardsDemo></InfiniteMovingCardsDemo>
